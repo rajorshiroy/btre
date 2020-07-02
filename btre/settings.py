9a98fc14dbe72b74e2676ed76f3d9fd12a33f064
Rajorshi <rajorshi.me@gmail.com>
2020-07-03 00:15:15 +0530

    added postgres database

--- btre/settings.py
+++ btre/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +76,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Chickencurry1',
+        'HOST': 'localhost',
     }
 }
 
